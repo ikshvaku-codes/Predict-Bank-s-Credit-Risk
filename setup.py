74d5d76552ca8c6be2d0b05ad8030f40729bba15
ikshvaku <be.divanshu@gmail.com>
2023-01-09 10:41:48 +0530

    Bug Fix in setup.py readline -> readlines

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 def get_requirements():
     with open("requirements.txt", "r") as f:
-        line = f.readline()
+        line = f.readlines()
         requirement_list = [requirement.replace("\n", "") for requirement in line]
         if "-e ." in requirement_list:
             requirement_list.remove("-e .")
@@ -13,7 +13,7 @@ def get_requirements():
 
 
 PROJECT_NAME = "Predict Bank's Credit Risk"
-PROJECT_VERSION = "0.0.1"
+PROJECT_VERSION = "0.0.2"
 PROJECT_DESCRIPTION = "This is a machine learning model to predict Bank's Credit Risk"
 PROJECT_AUTHOR = "ikshvaku"
 PROJECT_DEPENDENCIES = get_requirements()
