45153a251a188b457a03b714244577174eca8859
ikshvaku <be.divanshu@gmail.com>
2023-01-09 10:42:11 +0530

    Add code for logger

---
+++ predict/logger/__init__.py
@@ -0,0 +1,19 @@
+import logging, os
+from datetime import datetime
+
+LOG_DIR = "predict_logs"
+
+CURRENT_TIMESTAMP = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+FILE_NAME = f'log_{CURRENT_TIMESTAMP}.log'
+
+FILE_PATH = os.path.join(LOG_DIR, FILE_NAME)
+
+logging.basicConfig(level=logging.info, 
+                    filename=FILE_PATH,
+                    filemode='w',
+                    format="""[%(asctime)s] %(name)s 
+                                            - %(levelname)s 
+                                            - %(message)s""")
