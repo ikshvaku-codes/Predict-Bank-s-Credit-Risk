363b5c46ef6941f21c89d621fdde0b2df76cd377
ikshvaku <be.divanshu@gmail.com>
2023-02-17 11:01:40 +0530

    Fix bugs in predict API

--- predict/entity/CreditRisk.py
+++ predict/entity/CreditRisk.py
@@ -3,31 +3,31 @@ import sys
 
 from predict.exception import PredictException
 from predict.util import load_object
-
+from predict.component.ModelTrainer import predictEstimatorModel
 import pandas as pd
 
 class CreditRisk:
     def __init__(self,
-        status,
-        duration,
-        credit_history,
-        purpose,
-        amount,
-        savings,
-        employment_duration,
-        installment_rate,
-        personal_status_sex,
-        other_debtors,
-        present_residence,
-        prop,
-        age,
-        other_installment_plans,
-        housing,
-        number_credits,
-        job,
-        people_liable,
-        telephone,
-        foreign_worker
+        status:float,
+        duration:float,
+        credit_history:float,
+        purpose:float,
+        amount:float,
+        savings:float,
+        employment_duration:float,
+        installment_rate:float,
+        personal_status_sex:float,
+        other_debtors:float,
+        present_residence:float,
+        prop:float,
+        age:float,
+        other_installment_plans:float,
+        housing:float,
+        number_credits:float,
+        job:float,
+        people_liable:float,
+        telephone:float,
+        foreign_worker:float
     ):
         try:
             self.status = status 
@@ -63,26 +63,26 @@ class CreditRisk:
     def get_credit_risk_data_as_dict(self):
         try:
             input_data = {
-                "status":[self.status],
-                "duration":[self.duration],
-                "credit_history":[self.credit_history],
-                "purpose":[self.purpose],
-                "amount":[self.amount],
-                "savings":[self.savings],
-                "employment_duration":[self.employment_duration],
-                "installment_rate":[self.installment_rate],
-                "personal_status_sex":[self.personal_status_sex],
-                "other_debtors":[self.other_debtors],
-                "present_residence":[self.present_residence],
-                "prop":[self.prop],
-                "age":[self.age],
-                "other_installment_plans":[self.other_installment_plans],
-                "housing":[self.housing],
-                "number_credits":[self.number_credits],
-                "job":[self.job],
-                "people_liable":[self.people_liable],
-                "telephone":[self.telephone],
-                "foreign_worker":[self.foreign_worker]
+                "laufkont":[self.status],
+                "laufzeit":[self.duration],
+                "moral":[self.credit_history],
+                "verw":[self.purpose],
+                "hoehe":[self.amount],
+                "sparkont":[self.savings],
+                "beszeit":[self.employment_duration],
+                "rate":[self.installment_rate],
+                "famges":[self.personal_status_sex],
+                "buerge":[self.other_debtors],
+                "wohnzeit":[self.present_residence],
+                "verm":[self.prop],
+                "alter":[self.age],
+                "weitkred":[self.other_installment_plans],
+                "wohn":[self.housing],
+                "bishkred":[self.number_credits],
+                "beruf":[self.job],
+                "pers":[self.people_liable],
+                "telef":[self.telephone],
+                "gastarb":[self.foreign_worker]
                 }
             return input_data
         except Exception as e:
@@ -90,26 +90,51 @@ class CreditRisk:
         
         
 class CreditRiskPreditor:
-    def __init__(self, model_dir:str):
+    def __init__(self, model_dir:str, preprocessing_dir:str):
         try:
             self.model_dir = model_dir
+            self.preprocessing_dir = preprocessing_dir
         except Exception as e:
             raise PredictException(e, sys) from e
         
     def get_latest_model_path(self):
         try:
-            folder_name = list(map(int,os.listdir(self.model_dir)))
-            latest_model_dir = os.path.join(self.model_dir, f"{max(folder_name)}")
+            path_list = os.listdir(self.model_dir)
+            folder_name = {
+                int(value.replace("-","")): value for value in path_list
+            }
+            
+            latest_model_dir = os.path.join(self.model_dir, f"{folder_name[max(folder_name.keys())]}")
             file_name = os.listdir(latest_model_dir)[0]
             latest_model_path = os.path.join(latest_model_dir, file_name)
             return latest_model_path
         except Exception as e:
             raise PredictException(e, sys) from e
         
+    def get_latest_preprocessing_obj_path(self):
+        try:
+            path_list = os.listdir(self.preprocessing_dir)
+            folder_name = {
+                int(value.replace("-","")): value for value in path_list
+            }
+            
+            latest_preprocessing_dir = os.path.join(self.preprocessing_dir, f"{folder_name[max(folder_name.keys())]}", "preprocessed")
+            file_name = os.listdir(latest_preprocessing_dir)[0]
+            latest_preprocessing_dir = os.path.join(latest_preprocessing_dir, file_name)
+            return latest_preprocessing_dir
+        except Exception as e:
+            raise PredictException(e, sys) from e
+        
     def predict(self, X):
         try:
             model_path = self.get_latest_model_path()
+            preprocessing_obj_path = self.get_latest_preprocessing_obj_path()
+            
             model = load_object(model_path)
+            preprocessing_obj = load_object(preprocessing_obj_path)
+            
+            # estimated_credit_risk_obj = predictEstimatorModel(preprocessing_obj, model)
+            # estimated_credit_risk = estimated_credit_risk_obj.predict(X)
             estimated_credit_risk = model.predict(X)
             return estimated_credit_risk
         except Exception as e:
